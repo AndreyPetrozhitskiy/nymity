f09fccc6c25a702ba3ca3438823cd27f93da196c
AndreyPetrozhitskiy <super.okha2018@yandex.ru>
2024-05-16 01:14:30 +1000

    profile dev

--- src/app/feed/profile/[slug]/layout.tsx
+++ src/app/feed/profile/[slug]/layout.tsx
@@ -1,19 +1,6 @@
-import type { Metadata } from "next";
 import "@/src/styles/Profile/profile.scss";
-import { Manrope } from "next/font/google";
 import { getUsersId } from "@/src/Func/profile";
-const manrope = Manrope({
-  weight: ["200", "300", "400", "500", "600", "700", "800"],
-  style: "normal",
-  subsets: [
-    "cyrillic",
-    "cyrillic-ext",
-    "greek",
-    "latin",
-    "latin-ext",
-    "vietnamese",
-  ],
-});
+
 type Props = {
   params: {
     slug: number | string;
@@ -38,5 +25,5 @@ export default function ProfileLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className={`${manrope.className} ${"Profile"}`}>{children}</div>;
+  return <div className="Profile">{children}</div>;
 }
